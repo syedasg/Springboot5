f8e4211a1300c61c535460bf692497137a5443b9
Agha hussain <Agha hussain@DESKTOP-36KRLST>
2020-04-19 02:34:51 -0500

    Curse data commit 1

---
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,16 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository <Topic,String>
+{
+	
+	/* CRUD OPERATIONS */
+	//getAllTopics()
+	//getTopic(String id)
+	//addTopic(Topic topic)
+	//updateTopic(String id, Topic topic)
+	//deleteTopic(String id)
+	
+
+}
